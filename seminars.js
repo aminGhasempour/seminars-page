085f9c9349a30fb4d10bc6ea870cd8bd1ee37738
Mohammad Ghasempour <aghasempour@gmail.com>
2026-06-16 11:31:07 +0200

    Add files via upload

---
+++ seminars.js
@@ -0,0 +1,105 @@
+/*
+  Seminar data for Joint Statistical Seminars.
+
+  HOW TO EDIT
+  1. Add, remove, or edit seminar entries in the window.SEMINARS list below.
+  2. Keep date in YYYY-MM-DD format and time in HH:MM, 24-hour format.
+  3. The page automatically splits upcoming vs previous seminars based on today's date.
+
+  Keep this file in the same folder as index.html.
+*/
+
+// Add, remove, or update seminars here. Keep the same structure as the examples.
+
+window.SEMINARS = [
+                   {
+                       "id": "HuixiaWangs-licentiate-seminar ",
+                       "title": "Huixia Wang's licentiate seminar",
+                       "date": "2026-09-18",
+                       "startTime": "13:00",
+                       "endTime": "",
+                       "timezone": "Europe/Stockholm",
+                       "speaker": "Huixia Wang",
+                       "affiliation": "Department of Statistics",
+                       "venue": "Hörsal HUM.D.230 - Hohaj",
+                       "abstract": "To be announced",
+                       "tags": [
+                         "Statistics"
+                       ]
+                     },
+                   {
+                       "id": "2026-09-24-Reza-committee",
+                       "title": "To be announced",
+                       "date": "2026-09-24",
+                       "startTime": "13:00",
+                       "endTime": "14:00",
+                       "timezone": "Europe/Stockholm",
+                       "speaker": "To be announced",
+                       "affiliation": "To be announced",
+                       "venue": "SAM.A.241",
+                       "abstract": "To be announced",
+                       "tags": [
+                         "Statistics"
+                       ]
+                     },
+                   {
+                       "id": "2026-09-25-Reza-Seydi",
+                       "title": "Reza Seydi's dissertation",
+                       "date": "2026-09-25",
+                       "startTime": "09:30",
+                       "endTime": "12:00",
+                       "timezone": "Europe/Stockholm",
+                       "speaker": "Reza Seydi",
+                       "affiliation": "Department of Statistics",
+                       "venue": "NBET.A.101",
+                       "abstract": "To be announced",
+                       "tags": [
+                         "Statistics"
+                       ]
+                     },
+                   {
+                       "id": "2026-08-18-Reza-Seydi",
+                       "title": "To be announced",
+                       "date": "2026-08-18",
+                       "startTime": "13:00",
+                       "endTime": "14:00",
+                       "timezone": "Europe/Stockholm",
+                       "speaker": "Reza Seydi",
+                       "affiliation": "Department of Statistics",
+                       "venue": "SAM.A.233",
+                       "abstract": "To be announced",
+                       "tags": [
+                         "Statistics"
+                       ]
+                     },
+                     {
+                       "id": "2026-09-01-ye.tian",
+                       "title": "To be announced",
+                       "date": "2026-09-01",
+                       "startTime": "13:00",
+                       "endTime": "14:00",
+                       "timezone": "Europe/Stockholm",
+                       "speaker": "Ye Tian",
+                       "affiliation": "Department of Statistics",
+                       "venue": "SAM.A.257",
+                       "abstract": "To be announced",
+                       "tags": [
+                         "Statistics"
+                       ]
+                     },
+                     {
+                       "id": "2026-09-15-To-be-announced",
+                       "title": "To be announced",
+                       "date": "2026-09-15",
+                       "startTime": "13:00",
+                       "endTime": "13:30",
+                       "timezone": "Europe/Stockholm",
+                       "speaker": "To be announced",
+                       "affiliation": "Department of Statistics",
+                       "venue": "SAM.A.343",
+                       "abstract": "To be announced",
+                       "tags": [
+                         "Statistics"
+                       ]
+                     }
+];
